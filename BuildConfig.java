12aeda65048e1d4fa7ac7f48d07d928c59796b02
hecoez <85050973+hecoez@users.noreply.github.com>
2021-09-16 18:06:00 -0700

    Add files via upload

---
+++ BuildConfig.java
@@ -0,0 +1,9 @@
+package com.linkses.certifica;
+
+public final class BuildConfig {
+    public static final String APPLICATION_ID = "com.linkses.certifica";
+    public static final String BUILD_TYPE = "release";
+    public static final boolean DEBUG = false;
+    public static final int VERSION_CODE = 10004;
+    public static final String VERSION_NAME = "1.0.4";
+}
